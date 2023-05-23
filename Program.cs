0184b8e3178e79255191db4ad2ab997b694798fa
Krittapas Chinthumrucks (1000112540) <CHINK3@student.op.ac.nz>
2023-05-23 13:47:47 +1200

    Adding the instruction console

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace Game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the......... ");
+            helpConsole();
+        }
+        static void helpConsole()
+        {
+            Console.WriteLine("If you want to get an instruction of the game");
+            Console.WriteLine("Type help");
+            string userInput = Console.ReadLine();
+
+            if (userInput == "help" ||userInput  == "HELP") 
+            {
+                Console.WriteLine("Instruction");
+            }
+
+            
         }
     }
 }
